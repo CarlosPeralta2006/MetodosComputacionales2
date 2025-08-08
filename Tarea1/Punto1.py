a34b745c991350dffd96bb08dd8385c3dc7ef90f
CarlosPeralta2006 <c.peraltam@uniandes.edu.co>
2025-08-08 17:49:40 -0500

    Committt

--- Tarea1/Punto1.py
+++ Tarea1/Punto1.py
@@ -1,2 +1,10 @@
 
-este es el ejemplo
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import emcee
+import corner
+import os
+from google.colab import drive
+import scipy.optimize as spo
+import math
