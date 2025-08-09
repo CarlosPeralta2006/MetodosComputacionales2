dffe28576c3860e783cf12ae65ffcdbd05493afe
CarlosPeralta2006 <c.peraltam@uniandes.edu.co>
2025-08-08 19:09:48 -0500

    nnn

--- Tarea1/Punto1.py
+++ Tarea1/Punto1.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,3 +7,7 @@ import os
 from google.colab import drive
 import scipy.optimize as spo
 import math
+
+bash 
+
+pip install pandas numpy
