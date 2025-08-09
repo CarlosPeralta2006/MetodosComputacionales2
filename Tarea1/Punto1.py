f211dff9b1116f0ecad557c0789c12922b1ad678
CFelipeTR <carlosferivera@gmail.com>
2025-08-08 20:15:19 -0500

    777

--- Tarea1/Punto1.py
+++ Tarea1/Punto1.py
@@ -2,20 +2,24 @@ import glob
 import pandas as pd
 
 archivos = glob.glob("Data/mammography_spectra/Mo_unfiltered_10kV-50kV/*.dat")  # lista de archivos con búsqueda de la ruta
+
 Mo = {}
 for archivo in archivos:
     Mo_ind = pd.read_csv(archivo, sep=r"\s+", header=None, names=["Energía", "Fotones"], skiprows=15, encoding="latin1")
-    Mo[archivo] = Mo_ind
+    arch = archivo[52:-4]
+    Mo[arch] = Mo_ind
 
 archivos2 = glob.glob("Data/mammography_spectra/Rh_unfiltered_10kV-50kV/*.dat") 
 Rh = {}
 for archivo2 in archivos2:
     Rh_ind = pd.read_csv(archivo2, sep=r"\s+", header=None, names=["Energía", "Fotones"], skiprows=15, encoding="latin1")
-    Rh[archivo2] = Rh_ind
-    
-    
+    arch2 = archivo2[52:-4]
+    Rh[arch2] = Rh_ind
+
 archivos3 = glob.glob("Data/mammography_spectra/W_unfiltered_10kV-50kV/*.dat")
 W = {}
 for archivo3 in archivos3:
     W_ind = pd.read_csv(archivo3, sep=r"\s+", header=None, names=["Energía", "Fotones"], skiprows=15, encoding="latin1")
-    W[archivo3] = W_ind
+    arch3 = archivo3[50:-4]
+    W[arch3] = W_ind
+
