e1ff186f52e1c625914cdc0613c11b96a41710b4
CFelipeTR <carlosferivera@gmail.com>
2025-08-22 20:06:06 -0500

    zzz

--- Tarea_2/punto2.py
+++ Tarea_2/punto2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame as DF
+import numpy as np
 import matplotlib.pyplot as plt
 
 # - - - 2.a - - - - - - - - - 
@@ -8,8 +9,8 @@ data = pd.read_csv("Data2/tomography_data/SN_d_tot_V2.0.csv")
 data["spots"] = data["spots"].replace(-1, pd.NA)
 data["spot_std"] = data["spot_std"].replace(-1.0, pd.NA)
 
-data["spots"][0] = data["spots"].mean()
-data["spot_std"][0] = data["spot_std"].mean()
+data.loc[0, "spots"] = data["spots"].mean()
+data.loc[0, "spot_std"] = data["spot_std"].mean()
 
 data["spots"] = pd.to_numeric(data["spots"])
 data["spot_std"] = pd.to_numeric(data["spot_std"])
@@ -18,6 +19,26 @@ data["spot_std"] = pd.to_numeric(data["spot_std"])
 data["spots"] = data["spots"].interpolate(method="pchip")
 data["spot_std"] = data["spot_std"].interpolate(method="pchip")
 
+spots = data["spots"]
+ts = data["decimal_date"]
+
+
 # si solo se quitan, la periodicidad del muestreo se perdería
 
 # - - - 2.b - - - - - - - - - 
+
+param = 10
+F = np.fft.fft(spots, n = param * len(spots))
+F = np.fft.fftshift(F)
+
+freq = np.fft.fftfreq(n = param * len(spots) ,d = 1)
+freq = np.fft.fftshift(freq)
+
+
+F_filtrada = F.copy()
+F_filtrada[abs(freq) < 10] = 0.
+
+plt.plot(freq,abs(F))
+plt.plot(freq,abs(F_filtrada))
+
+plt.show()
