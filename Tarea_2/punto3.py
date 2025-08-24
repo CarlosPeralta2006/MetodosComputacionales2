0bfb6e1226cdb20462b99131a1b778707ca7ed75
CFelipeTR <carlosferivera@gmail.com>
2025-08-23 19:59:07 -0500

    punto 3 avance

--- Tarea_2/punto3.py
+++ Tarea_2/punto3.py
@@ -32,13 +32,28 @@ plt.savefig("3.a.pdf", bbox_inches="tight", pad_inches=0.)
 
 # - - - 3.b - - - - - - - - - 
 
+plt.rcParams["image.cmap"] = "gray"
+
 pato = np.array(Image.open('Data2/tomography_data/p_a_t_o.jpg'))
 yp, xp = pato.shape
-print(pato.shape)
-X,Y = np.meshgrid(np.arange(img.shape[1]),np.arange(img.shape[0]))
+X,Y = np.meshgrid(np.arange(pato.shape[1]),np.arange(pato.shape[0]))
 
+F = np.fft.fft2(pato)
+F = np.fft.fftshift(F)
+
+# cuadros a eliminar del espacio de frecuencias 
+Z = (abs(X-256) < 6) & (abs(Y-128) < 121) 
+W = (abs(X-256) < 6) & (abs(Y-384) < 121)
+G = (abs(X-128) < 121) & (abs(Y-256) < 6)
+E = (abs(X-384) < 121) & (abs(Y-256) < 6)
+Z_line = np.abs(Y - (-0.34*X + 340)) < 2.4
+Z_ring = (np.hypot(X-256,Y-256) <= 13.5) & (np.hypot(X-256,Y-256) >= 4.9)
+
+F_filt = F * (1-Z) * (1-W) * (1-G) * (1-E) * (1-Z_line) * (1-Z_ring)
+
+pato_filt = np.fft.ifft2(np.fft.fftshift(F_filt))
 
 plt.figure(figsize=(12,12))
-plt.imshow(np.clip(img_filt, 0, 255).astype(np.uint8))  
+plt.imshow(pato_filt.real)  
 plt.axis("off")                   
 plt.savefig("3.b.a.pdf", bbox_inches="tight", pad_inches=0.)
