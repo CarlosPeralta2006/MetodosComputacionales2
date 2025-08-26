82a7505649805265f1abf626b878b9960c92b779
CFelipeTR <carlosferivera@gmail.com>
2025-08-25 22:42:54 -0500

    sueñoooo

--- Tarea_2/punto3.py
+++ Tarea_2/punto3.py
@@ -42,18 +42,18 @@ F = np.fft.fft2(pato)
 F = np.fft.fftshift(F)
 
 # cuadros a eliminar del espacio de frecuencias - tanteo
-Z = (abs(X-256) < 6) & (abs(Y-128) < 121) 
-W = (abs(X-256) < 6) & (abs(Y-384) < 121) 
-H = (abs(X-128) < 121) & (abs(Y-256) < 6)
-E = (abs(X-384) < 121) & (abs(Y-256) < 6)
-Z_line = np.abs(Y - (-0.34*X + 340)) < 2.4
-Z_line2 = np.abs(Y - (X)) < 7.2
-Z_ring = (np.hypot(X-256,Y-256) <= 13.5) & (np.hypot(X-256,Y-256) >= 4.9)
+Z = (abs(X-256) < 7.5) & (abs(Y-128) < 121) 
+W = (abs(X-256) < 7.5) & (abs(Y-384) < 121) 
+H = (abs(X-128) < 121) & (abs(Y-256) < 7)
+E = (abs(X-384) < 121) & (abs(Y-256) < 7)
+Z_line = np.abs(Y - (-0.34*X + 340)) < 12
+Z_line2 = np.abs(Y - (X)) < 10
+Z_ring = (np.hypot(X-256,Y-256) <= 13.5) & (np.hypot(X-256,Y-256) >= 4)
 
-let = np.hypot(X-256,Y-256) <= 4.89
+let = np.hypot(X-256,Y-256) <= 4.4
 
-F_del = (1-Z) * (1-W) * (1-H) * (1-E) * (1-Z_line) * (1-Z_ring) * (1-Z_line2)
-F_fil = (F_del) | (let)
+F_del = (1-0.95*Z) * (1-0.95*W) * (1-H) * (1-E) * (1-0.95*Z_line) * (1-0.7*Z_ring) * (1-0.8*Z_line2)
+F_fil = np.maximum(F_del, let.astype(float))
 F_filt = F * F_fil
 
 pato_filt = np.fft.ifft2(np.fft.ifftshift(F_filt))
@@ -61,9 +61,9 @@ pato_filt = np.fft.ifft2(np.fft.ifftshift(F_filt))
 plt.figure(figsize=(12,12))
 plt.imshow(pato_filt.real)  
 plt.axis("off")                   
-plt.savefig("3.b.a.pdf", bbox_inches="tight", pad_inches=0.)
+plt.savefig("3.b.a.jpg", bbox_inches="tight", pad_inches=0.)
 
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 gato = np.array(Image.open('Data2/tomography_data/g_a_t_o.png'))
 yg, xg = gato.shape
@@ -73,20 +73,20 @@ Ga = np.fft.fft2(gato)
 Ga = np.fft.fftshift(Ga)
 
 #cuadros a eliminar del espacio de frecuencias - tanteo
-Z_linea1 = np.abs(Y - (1.99*X - 370)) < 6
-Z_linea2 = np.abs(Y - (-1.99*X + 1120)) < 2
-Z_linea3 = np.abs(Y - (375)) < 2
-Z_ring1 = (np.hypot(X-375.5,Y-377) <= 15.5) & (np.hypot(X-375.5,Y-377) >= 4.85)
+Z_linea1 = np.abs(y - (2*x - 370)) < 42  # principal fuente de interferencia
+Z_linea2 = np.abs(y - (-2*x + 1120)) < 5
+Z_linea3 = np.abs(y - (375)) < 2.1
+Z_ring1 = (np.hypot(x-375.5,y-377) <= 23.5) & (np.hypot(x-375.5,y-377) >= 4.85)
 
-let1 = np.hypot(X-375.5,Y-377) <= 5.5
+let1 = np.hypot(x-375.5,y-377) <= 9.5
 
-G_del = (1-Z_linea1) * (1-Z_linea2)  * (1-Z_linea3) * (1-0.7*Z_ring1)
+G_del = (1-0.98*Z_linea1) * (1-0.95*Z_linea2)  * (1-0.75*Z_linea3) * (1-0.96*Z_ring1)
 G_fil = np.maximum(G_del, let1.astype(float))
 G_filt = Ga * G_fil
 
 gato_filt = np.fft.ifft2(np.fft.ifftshift(G_filt))
 
 plt.figure(figsize=(12,12))
-plt.imshow(pato_filt.real)  
+plt.imshow(gato_filt.real)  
 plt.axis("off")                   
-plt.savefig("3.b.b.pdf", bbox_inches="tight", pad_inches=0.)
+plt.savefig("3.b.b.png", bbox_inches="tight", pad_inches=0.)
